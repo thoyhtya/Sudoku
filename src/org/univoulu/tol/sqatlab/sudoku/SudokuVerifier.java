c82c31014b01b0adb8e1a058f6b10ea6363f2fe5
somename <someemail>
2015-09-30 15:25:02 +0300

    besouro automatic message

--- src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/SudokuVerifier.java
@@ -3,6 +3,9 @@ package org.univoulu.tol.sqatlab.sudoku;
 public class SudokuVerifier {
 
 	public int verify(String candidateSolution) {
+		if(candidateSolution.length() != 81){
+			throw new InvalidParameterException("Candidate should be 81 characters");
+		}
 		// returns 1 if the candidate solution is correct
 		return 0;
 	}
--- src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
+++ src/org/univoulu/tol/sqatlab/sudoku/TestSudokuVerifier.java
@@ -1,14 +1,19 @@
 package org.univoulu.tol.sqatlab.sudoku;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
 public class TestSudokuVerifier {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testVerify() {
+		SudokuVerifier verifier = new SudokuVerifier();
+	}
+	
+	@Test
+	public void testVerifyLength1Return() {
+		SudokuVerifier verifier = new SudokuVerifier();
+		int result = verifier.verify("a");
 	}
 
 }
